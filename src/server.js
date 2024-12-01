ba6857b8c2149f0b7c2a2f19f0a640c8c7d1c7bb
tuanphampp9 <tuanphampp9@gmail.com>
2024-12-01 21:36:07 +0700

    config structure folder

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
-import express from 'express';
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const app = express();
-const hostName = 'localhost';
-const port = 8017;
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
 
-app.listen(port, hostName, () => {
-  console.log(`Server is running at http://${hostName}:${port}`);
-});
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Tuan pham pro, I am running at http://${ hostname }:${ port }/`)
+})
